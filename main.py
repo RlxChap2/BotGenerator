9e1b47f546c441462566c4658a2904ad9cef2528
RlxChap2 <133292635+RlxChap2@users.noreply.github.com>
2023-12-27 17:54:06 +0300

    v1.0.0

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import zipfile
+import requests
+from colorama import Fore, Style
+
+def download_extract_repo(repo_url):
+    response = requests.get(repo_url)
+    if response.status_code == 200:
+        with open('temp.zip', 'wb') as f:
+            f.write(response.content)
+        
+        with zipfile.ZipFile('temp.zip', 'r') as zip_ref:
+            zip_ref.extractall('.')
+        
+        os.remove('temp.zip')
+        print(Fore.GREEN + "Project downloaded and extracted successfully!" + Style.RESET_ALL)
+    else:
+        print(Fore.RED + "Failed to download the project." + Style.RESET_ALL)
+
+def main():
+    print(Fore.BLUE + "Choose a bot project:" + Style.RESET_ALL)
+    print(Fore.BLUE + "1. BotGenerator" + Style.RESET_ALL)
+    print(Fore.BLUE + "2. Tutorial" + Style.RESET_ALL)
+    
+    choice = input("Enter your choice: ")
+    
+    if choice == '1':
+        print(Fore.YELLOW + "Downloading BotGenerator..." + Style.RESET_ALL)
+        download_extract_repo('https://github.com/RlxChap2/BotGenerator/archive/refs/heads/main.zip')
+    elif choice == '2':
+        print(Fore.YELLOW + "Downloading Tutorial..." + Style.RESET_ALL)
+        download_extract_repo('https://github.com/RlxChap2/Tutorial/archive/refs/heads/main.zip')
+    elif choice == '3':
+        print(Fore.YELLOW + "Downloading Tutorial..." + Style.RESET_ALL)
+        download_extract_repo('https://github.com/RlxChap2/Tutorial/archive/refs/heads/main.zip')
+    elif choice == '4':
+        print(Fore.YELLOW + "Downloading Tutorial..." + Style.RESET_ALL)
+        download_extract_repo('https://github.com/RlxChap2/Tutorial/archive/refs/heads/main.zip')
+    else:
+        print(Fore.RED + "Invalid choice." + Style.RESET_ALL)
+
+if __name__ == "__main__":
+    main()
---
+++ tools/update.py
@@ -0,0 +1,53 @@
+import os
+import sys
+from time import sleep
+from zipfile import ZipFile
+
+import requests
+
+
+class Update():
+    def __init__(self):
+        self.version = '1.0.0'
+        self.github = 'https://raw.githubusercontent.com/RlxChap2/BotGenerator/main/tools/update.py'
+        self.zipfile = 'https://github.com/RlxChap2/BotGenerator/archive/refs/heads/main.zip'
+        self.update_checker()
+
+    def update_checker(self):
+        code = requests.get(self.github).text
+        if "self.version = '1.0.0'" in code:
+            print('This version is up to date!')
+            sleep(1)
+            print('You can now open builder.pyw to open the builder!')
+            sleep(1)
+            print('Exiting...')
+            sleep(2)
+            sys.exit()
+        else:
+            print('''
+                    ███╗   ██╗███████╗██╗    ██╗    ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗██╗
+                    ████╗  ██║██╔════╝██║    ██║    ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██║
+                    ██╔██╗ ██║█████╗  ██║ █╗ ██║    ██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗  ██║
+                    ██║╚██╗██║██╔══╝  ██║███╗██║    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  ╚═╝
+                    ██║ ╚████║███████╗╚███╔███╔╝    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗██╗
+                    ╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝      ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝
+                                      Your version of Bots Generator is outdated!''')
+            choice = input('\nWould you like to update? (y/n): ')
+            if choice.lower() == 'y':
+                new_version_source = requests.get(self.zipfile)
+                with open("Luna-Grabber-main.zip", 'wb')as zipfile:
+                    zipfile.write(new_version_source.content)
+                with ZipFile("Luna-Grabber-main.zip", 'r') as filezip:
+                    filezip.extractall(path=os.path.join(os.path.expanduser("~"), "Desktop"))
+                os.remove("Luna-Grabber-main.zip")
+                print('The new version is now on your desktop.\nUpdate Complete!')
+                print("Exiting...")
+                sleep(5)
+            if choice.lower() == 'n':
+                print('Exiting...')
+                sleep(2)
+                sys.exit()
+
+
+if __name__ == '__main__':
+    Update()
